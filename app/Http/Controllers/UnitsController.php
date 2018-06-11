4c871f5be6daa8b7a8f71a7d8aad4cece07118cf
Daron Adkins <daron.adkins@gmail.com>
2018-06-11 11:10:19 -0500

    housekeeping

--- app/Http/Controllers/UnitsController.php
+++ app/Http/Controllers/UnitsController.php
@@ -28,38 +28,4 @@ class UnitsController extends Controller
     {
         return $unit->with('images', 'availability', 'amenities')->find($unit->id);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Unit  $unit
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Unit $unit)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Unit  $unit
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Unit $unit)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Unit  $unit
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Unit $unit)
-    {
-        //
-    }
 }
