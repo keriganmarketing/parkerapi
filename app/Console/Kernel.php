c5be31d90317407aa8917302d26865ac0cdda0e8
Daron Adkins <daron.adkins@gmail.com>
2018-08-13 16:32:46 -0500

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new Update)->everyMinute();
+        $schedule->job(new Update)->everyThirtyMinutes();
     }
 
     /**
