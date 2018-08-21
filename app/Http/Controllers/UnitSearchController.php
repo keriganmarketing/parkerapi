e7dfbdc35d553248e64b31ede67db2000a712ce6
Daron Adkins <daron.adkins@gmail.com>
2018-08-21 12:56:32 -0500

    wip

--- app/Http/Controllers/UnitSearchController.php
+++ app/Http/Controllers/UnitSearchController.php
@@ -21,27 +21,30 @@ class UnitSearchController extends Controller
         $location = $request->location ?? null;
         $type     = $request->type ?? null;
 
-        $units = Unit::with('availability', 'images', 'details', 'rates')
-               ->when($name, function ($query) use ($name) {
-                    return $query->where('name', 'like', $name);
-               })
-               ->when($checkIn, function ($query) use ($checkIn) {
-                    return $query->whereHas('availability', function ($query) use ($checkIn){
-                        return $query->where('arrival_date', '>=', $checkIn);
-                    });
-               })
-               ->when($checkOut, function ($query) use ($checkOut) {
-                    return $query->whereHas('availability', function ($query) use ($checkOut){
-                        return $query->where('arrival_date', '<=', $checkOut);
-                    });
-               })
-               ->when($location, function ($query) use ($location){
-                   return $query->where('location', $location);
-               })
-               ->when($type, function ($query) use ($type){
-                   return $query->where('type', $type);
-               })
-               ->paginate(36);
+        $units = Unit::with('details', 'rates')
+                    ->with(['images' => function ($query) {
+                        return $query->where('sort_order', 1);
+                    }])
+                   ->when($name, function ($query) use ($name) {
+                        return $query->where('name', 'like', $name);
+                   })
+                   ->when($checkIn, function ($query) use ($checkIn) {
+                        return $query->whereHas('availability', function ($query) use ($checkIn){
+                            return $query->where('arrival_date', '>=', $checkIn);
+                        });
+                   })
+                   ->when($checkOut, function ($query) use ($checkOut) {
+                        return $query->whereHas('availability', function ($query) use ($checkOut){
+                            return $query->where('arrival_date', '<=', $checkOut);
+                        });
+                   })
+                   ->when($location, function ($query) use ($location){
+                       return $query->where('location', $location);
+                   })
+                   ->when($type, function ($query) use ($type){
+                       return $query->where('type', $type);
+                   })
+                   ->paginate(36);
 
         $units->appends($request->all())->links();
 
