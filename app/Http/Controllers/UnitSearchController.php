4e65f0da578a93415799ad639ee5590f79a9f5cc
Daron Adkins <daron.adkins@gmail.com>
2018-08-24 13:29:08 -0500

    wip

--- app/Http/Controllers/UnitSearchController.php
+++ app/Http/Controllers/UnitSearchController.php
@@ -31,53 +31,52 @@ class UnitSearchController extends Controller
                     ->with(['images' => function ($query) {
                         return $query->where('sort_order', 1);
                     }])
-                   /* ->when($name, function ($query) use ($name) { */
-                   /*      return $query->where('name', 'like', $name); */
-                   /* }) */
-                   /* ->when($checkIn, function ($query) use ($checkIn) { */
-                   /*      return $query->whereDoesntHave('availability', function ($query) use ($checkIn){ */
-                   /*          return $query->whereDate('arrival_date', '>=', $checkIn)->orWhereDate('departure_date', '<=', $checkIn); */
-                   /*      }); */
-                   /* }) */
-                   /* ->when($checkOut, function ($query) use ($checkOut) { */
-                   /*      return $query->whereDoesntHave('availability', function ($query) use ($checkOut){ */
-                   /*          return $query->whereDate('arrival_date', '>=', $checkOut)->orWhereDate('departure_date', '<=', $checkOut); */
-                   /*      }); */
-                   /* }) */
-                   /* ->when($location, function ($query) use ($location) { */
-                   /*      return $query->whereHas('searchCriteria', function ($query) use ($location){ */
-                   /*          return $query->where('name', 'like', $location); */
-                   /*      }); */
-                   /* }) */
-                   /* ->when($type, function ($query) use ($type) { */
-                   /*      return $query->whereHas('searchCriteria', function ($query) use ($type){ */
-                   /*          return $query->where('name', 'like', $type); */
-                   /*      }); */
-                   /* }) */
-                   /* ->when($pool, function ($query) { */
-                   /*     return $query->whereHas('amenities', function ($query) { */
-                   /*         return $query->where('rns_id', 45)->where('description', '!=', 'No'); */
-                   /*     }); */
-                   /* }) */
-                   /* ->when($internet, function ($query) { */
-                   /*     return $query->whereHas('amenities', function ($query) { */
-                   /*         return $query->where('rns_id', 22)->where('description', '!=', 'No'); */
-                   /*     }); */
-                   /* }) */
-                   /* ->when($linens, function ($query) { */
-                   /*     return $query->whereHas('amenities', function ($query) { */
-                   /*         return $query->where('rns_id', 14)->where('description', '!=', 'No'); */
-                   /*     }); */
-                   /* }) */
-                   /* ->when($pets, function ($query) { */
-                   /*     return $query->whereHas('amenities', function ($query) { */
-                   /*         return $query->where('rns_id', 50)->where('description', '!=', 'No'); */
-                   /*     }); */
-                   /* }) */
-                   /* ->paginate(36); */
+                   ->when($name, function ($query) use ($name) {
+                        return $query->where('name', 'like', $name);
+                   })
+                   ->when($checkIn, function ($query) use ($checkIn) {
+                        return $query->whereDoesntHave('availability', function ($query) use ($checkIn){
+                            return $query->whereDate('arrival_date', '>=', $checkIn)->orWhereDate('departure_date', '<=', $checkIn);
+                        });
+                   })
+                   ->when($checkOut, function ($query) use ($checkOut) {
+                        return $query->whereDoesntHave('availability', function ($query) use ($checkOut){
+                            return $query->whereDate('arrival_date', '>=', $checkOut)->orWhereDate('departure_date', '<=', $checkOut);
+                        });
+                   })
+                   ->when($location, function ($query) use ($location) {
+                        return $query->whereHas('searchCriteria', function ($query) use ($location){
+                            return $query->where('name', 'like', $location);
+                        });
+                   })
+                   ->when($type, function ($query) use ($type) {
+                        return $query->whereHas('searchCriteria', function ($query) use ($type){
+                            return $query->where('name', 'like', $type);
+                        });
+                   })
+                   ->when($pool, function ($query) {
+                       return $query->whereHas('amenities', function ($query) {
+                           return $query->where('rns_id', 45)->where('description', '!=', 'No');
+                       });
+                   })
+                   ->when($internet, function ($query) {
+                       return $query->whereHas('amenities', function ($query) {
+                           return $query->where('rns_id', 22)->where('description', '!=', 'No');
+                       });
+                   })
+                   ->when($linens, function ($query) {
+                       return $query->whereHas('amenities', function ($query) {
+                           return $query->where('rns_id', 14)->where('description', '!=', 'No');
+                       });
+                   })
+                   ->when($pets, function ($query) {
+                       return $query->whereHas('amenities', function ($query) {
+                           return $query->where('rns_id', 50)->where('description', '!=', 'No');
+                       });
+                   })
+                   ->paginate(36);
 
-        /* $units->appends($request->all())->links(); */
-                    ->get();
+        $units->appends($request->all())->links();
 
         return $units;
     }
